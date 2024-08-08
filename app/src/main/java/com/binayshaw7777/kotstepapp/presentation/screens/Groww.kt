a285bdaf1464e43900bf5e97dfbba94d2d4eb46c
Binay Shaw <binayshaw7777@gmail.com>
2024-08-08 10:33:13 +0530

    Wrapped up the Groww UI

--- app/src/main/java/com/binayshaw7777/kotstepapp/presentation/screens/Groww.kt
+++ app/src/main/java/com/binayshaw7777/kotstepapp/presentation/screens/Groww.kt
@@ -14,6 +14,9 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.ChevronRight
+import androidx.compose.material.icons.filled.Help
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -23,13 +26,16 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Vertices
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.binayshaw7777.kotstepapp.presentation.ui.theme.GROWW_BACKGROUND
@@ -95,6 +101,8 @@ fun GrowwContent(modifier: Modifier = Modifier, paddingValues: PaddingValues) {
                 )
             )
 
+            Spacer(modifier = Modifier.height(8.dp))
+
             Text(
                 text = "New SIP   ∙   Completed", style = TextStyle(
                     fontFamily = NOTO_SANS_FONT_FAMILY,
@@ -112,7 +120,7 @@ fun GrowwContent(modifier: Modifier = Modifier, paddingValues: PaddingValues) {
                 Text(
                     text = "HDFC Mid Cap Opportunities Direct Plan  Growth", style = TextStyle(
                         fontFamily = ROBOTO_FONT_FAMILY,
-                        fontWeight = FontWeight.Medium,
+                        fontWeight = FontWeight.SemiBold,
                         fontSize = 12.sp,
                         color = Color.LightGray
                     )
@@ -128,5 +136,129 @@ fun GrowwContent(modifier: Modifier = Modifier, paddingValues: PaddingValues) {
         }
 
         HorizontalDivider(color = Color.DarkGray, modifier = Modifier.padding(vertical = 24.dp))
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            GrowwSideCard(
+                modifier = Modifier.weight(1f),
+                title = "Completed on",
+                text = "02 Aug '24"
+            )
+            VerticalDivider(
+                modifier = Modifier
+                    .height(48.dp)
+                    .padding(horizontal = 16.dp)
+            )
+            GrowwSideCard(modifier = Modifier.weight(1f), title = "NAV Date", text = "01 Aug '24")
+        }
+
+        HorizontalDivider(color = Color.DarkGray, modifier = Modifier.padding(vertical = 24.dp))
+
+        Text(
+            text = "Status", style = TextStyle(
+                fontFamily = ROBOTO_FONT_FAMILY,
+                fontWeight = FontWeight.Bold,
+                fontSize = 14.sp,
+                color = Color.White
+            ),
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+
+        HorizontalDivider(color = Color.DarkGray, modifier = Modifier.padding(vertical = 24.dp))
+
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        ) {
+            Text(
+                text = "Details", style = TextStyle(
+                    fontFamily = ROBOTO_FONT_FAMILY,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 14.sp,
+                    color = Color.White
+                )
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Icon(
+                imageVector = Icons.Default.KeyboardArrowDown,
+                tint = Color.White,
+                contentDescription = null,
+                modifier = Modifier.size(20.dp)
+            )
+        }
+
+        HorizontalDivider(color = Color.DarkGray, modifier = Modifier.padding(vertical = 24.dp))
+
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(horizontal = 16.dp)) {
+            Icon(
+                imageVector = Icons.Filled.Help,
+                tint = Color.White,
+                contentDescription = null,
+            )
+            Text(
+                text = "Need Help?", style = TextStyle(
+                    fontFamily = ROBOTO_FONT_FAMILY,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 14.sp,
+                    color = Color.White
+                ),
+                modifier = Modifier.padding(start = 8.dp)
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Icon(
+                imageVector = Icons.Default.KeyboardArrowDown,
+                tint = Color.White,
+                contentDescription = null,
+                modifier = Modifier.size(16.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun GrowwSideCard(
+    modifier: Modifier = Modifier,
+    title: String,
+    text: String
+) {
+    Column(
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .then(modifier)
+    ) {
+        Text(
+            text = title, style = TextStyle(
+                fontFamily = ROBOTO_FONT_FAMILY,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 12.sp,
+                color = Color.LightGray
+            )
+        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = text, style = TextStyle(
+                    fontFamily = ROBOTO_FONT_FAMILY,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 14.sp,
+                    color = Color.White
+                )
+            )
+            Icon(
+                imageVector = Icons.Outlined.Info,
+                tint = Color.White,
+                contentDescription = null,
+                modifier = Modifier.size(16.dp)
+            )
+        }
     }
 }
