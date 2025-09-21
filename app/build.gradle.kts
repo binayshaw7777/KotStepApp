8dc1e59f4ef67084905b13f23adbf75a6583ccb3
Binay Shaw <binayshaw7777@gmail.com>
2025-09-21 14:47:27 +0530

    Refactor: Upgrade dependencies and project settings
    This commit updates the project with the following changes:
    
    - Upgraded AGP, Kotlin, KotStep, Compose BOM, and other dependencies.
    - Enabled Kotlin Compose plugin.
    - Enabled strong skipping mode for Compose compiler.
    - Added various Compose preview inspection tools.
    - Updated Gradle JVM arguments for improved performance and stability.
    - Removed outdated `.idea/other.xml` file.
    - Configured test runner to be chosen per test in `gradle.xml`.
    - Updated `.idea/kotlinc.xml` to reflect Kotlin version 2.0.0.
    - Removed XML declaration from `.idea/misc.xml`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -36,11 +37,8 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+    composeCompiler {
+        enableStrongSkippingMode = true
     }
     packaging {
         resources {
